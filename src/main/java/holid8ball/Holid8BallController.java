de29cd5775b95d9575511a25530850e5345d3109
Kyle Downey <kjdowney.github@gmail.com>
2016-01-25 19:54:16 +0000

    Initial implementation: Takes a holiday name directly from user.

---
+++ src/main/java/holid8ball/Holid8BallController.java
@@ -0,0 +1,19 @@
+package holid8ball;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Holid8BallController {
+
+    private static final String template = "%s";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/magicholid8ball")
+    public Holiday holiday(@RequestParam(value="name", defaultValue="New Year's Day") String name) {
+        return new Holiday(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
---
+++ src/main/java/holid8ball/Holiday.java
@@ -0,0 +1,20 @@
+package holid8ball;
+
+public class Holiday {
+
+    private final long id;
+    private final String name;
+
+    public Holiday(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

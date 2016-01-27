abb6bfeba471696f5681ba6e5bc8100a8e3fc406
Kyle Downey <kjdowney.github@gmail.com>
2016-01-27 23:59:14 +0000

    Add schema version information to Holiday class.

--- src/main/java/holid8ball/Holiday.java
+++ src/main/java/holid8ball/Holiday.java
@@ -3,10 +3,12 @@ package holid8ball;
 public class Holiday {
 
    private final long id;
+   private final long schemaVersion;
    private final String name;
    private final String monthName;
 
    public Holiday(long id, String name, String monthName) {
+      this.schemaVersion = 3;
       this.id = id;
       this.name = name;
       this.monthName = monthName;
@@ -14,6 +16,10 @@ public class Holiday {
 
    public long getId() {
       return id;
+   }
+
+   public long getSchemaVersion() {
+      return schemaVersion;
    }
 
    public String getName() {

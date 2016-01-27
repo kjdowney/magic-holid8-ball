b23f5eb304cc56b8e577ac344ef6a83ca0ce301a
Kyle Downey <kjdowney.github@gmail.com>
2016-01-27 02:05:11 +0000

    Add a holiday for all months and randomly select one by default.

--- src/main/java/holid8ball/Holid8BallController.java
+++ src/main/java/holid8ball/Holid8BallController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @RestController
 public class Holid8BallController {
 
@@ -18,12 +20,48 @@ public class Holid8BallController {
          String name = "Unknown";
 
          if ("ANY".equalsIgnoreCase(monthName)) {
-            monthNum = 1;
+            monthNum = ThreadLocalRandom.current().nextInt(0, 12);
          }
 
-         if (1 == monthNum) {
-            monthName = "January";
-            name = "New Year's Day";
+         switch(monthNum) {
+            case 1:  monthName = "January";
+                     name = "New Year's Day";
+                     break;
+            case 2:  monthName = "February";
+                     name = "Valentine's Day";
+                     break;
+            case 3:  monthName = "March";
+                     name = "St. Patrick's Day";
+                     break;
+            case 4:  monthName = "April";
+                     name = "April Fool's Day";
+                     break;
+            case 5:  monthName = "May";
+                     name = "Cinco de Mayo";
+                     break;
+            case 6:  monthName = "June";
+                     name = "Flag Day";
+                     break;
+            case 7:  monthName = "July";
+                     name = "Independence Day";
+                     break;
+            case 8:  monthName = "August";
+                     name = "Purple Heart Day";
+                     break;
+            case 9:  monthName = "September";
+                     name = "Labor Day";
+                     break;
+            case 10: monthName = "October";
+                     name = "Columbus Day";
+                     break;
+            case 11: monthName = "November";
+                     name = "Thanksgiving";
+                     break;
+            case 12: monthName = "December";
+                     name = "Christmas Day";
+                     break;
+            default: monthName = "Invalid month";
+                     break;
          }
 
          return new Holiday(counter.incrementAndGet(),
